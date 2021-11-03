8a0cf50fbd0190a236f8dc4947df007d2848e906
nicolas <nicolasterramaia@gmail.com>
2021-11-03 08:36:52 -0300

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        static private Customer customer1 = new Customer { Name = "Nícolas", Id = 1};
+        static private Customer customer2 = new Customer { Name = "Lucas", Id = 2 };
+        private List<Customer> customers= new List<Customer> { customer1, customer2 };
+        
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customersListViewModel = new CustomersListViewModel { customers = customers };
+
+            return View(customersListViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var specificCustomer= customers.Find(customer => customer.Id == id);
+            return View(specificCustomer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name= "Shrek" };
+            var customers = new List<Customer>
+             {
+                 new Customer{Name = "Customer 1"},
+                 new Customer{Name = "Customer 2"}
+             };
+
+            var viewModel = new RandomMovieViewModel { Movie = movie, Customers = customers };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index()
+        {
+            var movie = new Movie { Name = "Shrek" };
+            var movie2 = new Movie { Name = "Spiderman" };
+            var movies = new List<Movie>
+            {
+                movie, movie2
+            };
+
+
+            var moviesViewModel = new MoviesListViewModel { movies = movies };
+
+            return View(moviesViewModel);
+
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
